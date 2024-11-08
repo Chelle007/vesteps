45d099c5bba6c79ea8afb1abb1c7b88a839f7521
desraymondz <desraymondz@gmail.com>
2024-11-08 18:10:37 +0800

    Added portal
    - will update inLevel variable to determine the next level

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 function preload(){
-    treasureOpenImg = loadImage("assets/treasure_open.png")
-    treasureCloseImg = loadImage("assets/treasure_close.png")
+    treasureOpenImg = loadImage("assets/treasure_open.png");
+    treasureCloseImg = loadImage("assets/treasure_close.png");
+    portalImg = loadImage("assets/portal.png");
 }
 
 function setup() {
@@ -11,8 +12,14 @@ function setup() {
         color: "#CC0000"
     }
     
+    // To determine which level is the user in
+    inLevel = 1;
+
     // TODO: change this later according to the state of the game functions
     keyIsFound = false;
+
+    // Make every image centered
+    imageMode(CENTER);
 }
 
 function draw() {
@@ -32,6 +39,8 @@ function draw() {
 
     // Draw treasure according to figma (outside canvas)
     drawTreasure(width / 6 * 7, height / 10 * 3, keyIsFound);
+
+    drawPortal(width / 6 * 1, height / 10 * 1);
 }
 
 function windowResized() {
