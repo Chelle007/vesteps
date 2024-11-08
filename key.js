50a7f62038d2b1c02b09bfdd29cba836f3811cf3
desraymondz <desraymondz@gmail.com>
2024-11-08 22:44:15 +0800

    Designed level 1
    next task:
    - make the character move and change node design

--- key.js
+++ key.js
@@ -1,18 +1,21 @@
 function drawKey(x, y, state) {
     if (!state) {
+        push();
+        translate(-40, -60);
         stroke(165, 42, 42);
         strokeWeight(4);
         fill(250, 255, 0);
-
+        
         // key body
         rect(x + 70, y - 10, 20, 40);
         rect(x + 45, y - 10, 15, 30);
         rect(x - 5, y - 10, 100, 20);
-
+        
         // key handle
         ellipse(x, y, 50, 50);
-
+        
         noStroke();
         noFill();
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -6,11 +6,14 @@ function preload(){
 
 var gameChar;
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
+// Get the nav height after it is loaded
 
-    let gameChar_x = width / 2;
-    let gameChar_y = (height / 10) * 7;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight - navHeight);
+
+    // let gameChar_x = width / 2;
+    // let gameChar_y = (height / 10) * 7;
 
     gameChar = {
         x: width / 2,
@@ -31,43 +34,80 @@ function setup() {
     // Make every image centered
     imageMode(CENTER);
     nodes = [];
-    nodes.push(new createNode(width / 2, (height / 5) * 3));
-    nodes.push(new createNode(width / 2, (height / 5) * 2));
-    nodes.push(new createNode(width / 6, (height / 5) * 2));
-    nodes.push(new createNode(width / 6, height / 5));
-    nodes.push(new createNode((width / 6) * 5, (height / 5) * 2));
-    nodes.push(new createNode((width / 6) * 7, (height / 5) * 2));
+    // start node
+    nodes.push(new createNode(width / 10 * 1, height / 10 * 5));
+    
+    // 2 going right nodes
+    nodes.push(new createNode(width / 10 * 3, height / 10 * 9));
+    nodes.push(new createNode(width / 10 * 5, height / 10 * 9));
+    
+    // 3 going up nodes
+    nodes.push(new createNode(width / 10 * 7, height / 10 * 5));
+    nodes.push(new createNode(width / 10 * 7, height / 10 * 7));
+    nodes.push(new createNode(width / 10 * 7, height / 10 * 9));
+    
+    // 2 going left nodes (go to chest)
+    nodes.push(new createNode(width / 10 * 5, height / 10 * 5));
+    nodes.push(new createNode(width / 10 * 3, height / 10 * 5));
+    nodes.push(new createNode(width / 10 * 1, height / 10 * 5));
+    
+    // 2 going up nodes
+    nodes.push(new createNode(width / 10 * 5, height / 10 * 3));
+    nodes.push(new createNode(width / 10 * 5, height / 10 * 1));
+    
+    // 2 going left nodes (to portal)
+    nodes.push(new createNode(width / 10 * 7, height / 10 * 1));
+    nodes.push(new createNode(width / 10 * 9, height / 10 * 1));
 }
 
 function draw() {
     background(200,200,200);
-    //draw guiding lines
-    for (let i = 0; i < 7; i++) {
-        stroke(255, 0, 0);
-        line((width / 6) * i, 0, (width / 6) * i, height);
-    };
 
+    ////////////////Stanley style guide lines////////////////////////
+    // // draw guiding lines
+    // for (let i = 0; i < 11; i++) {
+    //     stroke(255, 0, 0);
+    //     line((width / 10) * i, 0, (width / 10) * i, height);
+    // };
+    // //draw horizontal guiding lines
+    // for (let i = 0; i < 11; i++) {
+    //     stroke(255, 0, 0);
+    //     line(0, (height / 10) * i, width, (height / 10) * i);
+    // };
+
+    ////////////////Desmond style guide lines////////////////////////
+    //draw guiding lines
+    for (let i = 0; i < 6; i++) {
+        stroke(255, 0, 0);
+        line((width / 5) * i, 0, (width / 5) * i, height);
+    };
+    //draw horizontal guiding lines
     for (let i = 0; i < 11; i++) {
         stroke(255, 0, 0);
-        line(0, (height / 10) * i, width, (height / 10) * i)
+        line(0, (height / 5) * i, width, (height / 5) * i);
     };
+    /////////////////////////////////////////////////////////////////
 
     drawNodes();
 
-    // Draw among us
+    // Draw among us, update the gameChar.x and gameChar.y according to user's input
+    gameChar = {
+        x: width / 10 * 1,
+        y: height / 10 * 9,
+        color: "#CC0000"
+    }
+
+    // Draw game character on bottom left (starting point)
     amongus(gameChar.x, gameChar.y, gameChar.color);
 
     // Draw collectable (key)
-    drawKey(width / 6 * 5, height / 10 * 3, keyIsFound);
+    drawKey(width / 10 * 3, height / 10 * 5, keyIsFound);
 
     // Draw treasure (just for preview only)
-    drawTreasure(width / 6 * 5, height / 10 * 3, keyIsFound);
+    drawTreasure(width / 10 * 1, height / 10 * 5, keyIsFound);
 
-    // Draw treasure according to figma (outside canvas)
-    drawTreasure(width / 6 * 7, height / 10 * 3, keyIsFound);
-
-    drawPortal(width / 6 * 1, height / 10 * 1);
-    drawKey((width / 6) * 5, (height / 5) * 2, keyIsFound);
+    // Draw portal on top left corner (ending point)
+    drawPortal(width / 10 * 9, height / 10 * 1);
 }
 
 function windowResized() {
